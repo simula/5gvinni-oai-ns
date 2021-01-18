144b708a474292e29a48f6832788b0d75f48e47e
Thomas Dreibholz <dreibh@simula.no>
2021-01-18 11:11:41 +0100

    Some updates.

--- juju/p4ovscharm/reactive/p4ovscharm.py
+++ juju/p4ovscharm/reactive/p4ovscharm.py
@@ -55,182 +55,7 @@ vduHelper = VDUHelper.VDUHelper()
 
 
 # ###########################################################################
-# #### FlexRAN Charm functions                                           ####
-# ###########################################################################
-
-# ###### Installation #######################################################
-@when('sshproxy.configured')
-@when_not('flexrancharm.installed')
-def install_flexrancharm_proxy_charm():
-   set_flag('flexrancharm.installed')
-   vduHelper.setStatus('install_flexrancharm_proxy_charm: SSH proxy charm is READY')
-
-
-# ###### prepare-flexran-build function #####################################
-@when('actions.prepare-flexran-build')
-@when('flexrancharm.installed')
-@when_not('flexrancharm.prepared-flexran-build')
-def prepare_flexran_build():
-   vduHelper.beginBlock('prepare_flexran_build')
-   try:
-
-      # ====== Get FlexRAN parameters ===========================================
-      # For a documentation of the installation procedure, see:
-      # https://gitlab.eurecom.fr/mosaic5g/mosaic5g/-/wikis/tutorials/slicing
-
-      gitName       = function_get('git-name')
-      gitEmail      = function_get('git-email')
-      gitRepository = function_get('flexran-git-repository')
-      gitCommit     = function_get('flexran-git-commit')
-      gitDirectory  = 'mosaic5g'
-
-      flexranService_IPv4Interface = IPv4Interface(function_get('flexran-service-ipv4-interface'))
-      flexranService_IPv4Gateway   = IPv4Address(function_get('flexran-service-ipv4-gateway'))
-      if function_get('flexran-service-ipv6-interface') != '':
-         flexranService_IPv6Interface = IPv6Interface(function_get('flexran-service-ipv6-interface'))
-      else:
-         flexranService_IPv6Interface = None
-      if function_get('flexran-service-ipv6-gateway') != '':
-         flexranService_IPv6Gateway   = IPv6Address(function_get('flexran-service-ipv6-gateway'))
-      else:
-         flexranService_IPv6Gateway = None
-
-      # Prepare network configuration:
-      flexranService_IfName = 'ens4'
-      configurationService = vduHelper.makeInterfaceConfiguration(flexranService_IfName,
-                                                                  flexranService_IPv4Interface, flexranService_IPv4Gateway,
-                                                                  flexranService_IPv6Interface, flexranService_IPv6Gateway)
-
-      # ====== Prepare system ===============================================
-      vduHelper.beginBlock('Preparing system')
-      vduHelper.configureGit(gitName, gitEmail)
-      vduHelper.configureInterface(flexranService_IfName, configurationService, 61)
-      vduHelper.testNetworking()
-      vduHelper.waitForPackageUpdatesToComplete()
-      vduHelper.endBlock()
-
-      # ====== Prepare sources ==============================================
-      vduHelper.beginBlock('Preparing sources')
-      vduHelper.fetchGitRepository(gitDirectory, gitRepository, gitCommit)
-      vduHelper.executeFromString("""\
-cd /home/nornetpp/src/{gitDirectory} && \\
-git submodule init && \\
-git submodule update flexran
-""".format(gitDirectory = gitDirectory))
-      vduHelper.endBlock()
-
-
-      message = vduHelper.endBlock()
-      function_set( { 'outout': message } )
-      set_flag('flexrancharm.prepared-flexran-build')
-   except:
-      message = vduHelper.endBlockInException()
-      function_fail(message)
-   finally:
-      clear_flag('actions.prepare-flexran-build')
-
-
-# ###### configure-flexran function #########################################
-@when('actions.configure-flexran')
-@when('flexrancharm.prepared-flexran-build')
-def configure_flexran():
-   vduHelper.beginBlock('configure_flexran')
-   try:
-
-      # ====== Get FlexRAN parameters =======================================
-      # For a documentation of the installation procedure, see:
-      # https://gitlab.eurecom.fr/mosaic5g/mosaic5g/-/wikis/tutorials/slicing
-
-      gitDirectory = 'mosaic5g'
-
-      # ====== Build FlexRAN ================================================
-      vduHelper.beginBlock('Building FlexRAN itself')
-      vduHelper.executeFromString("""\
-export MAKEFLAGS="-j`nproc`" && \\
-cd /home/nornetpp/src/{gitDirectory} && \\
-mkdir -p logs && \\
-./build_m5g -f >logs/build_flexran.log 2>&1
-""".format(gitDirectory = gitDirectory))
-      vduHelper.endBlock()
-
-      # ====== Configure FlexRAN ================================================
-      vduHelper.beginBlock('Configuring FlexRAN')
-      vduHelper.executeFromString("""\
-cd /home/nornetpp/src/{gitDirectory}/flexran
-""".format(gitDirectory = gitDirectory))
-      vduHelper.endBlock()
-
-      # ====== Set up FlexRAN service ===========================================
-      vduHelper.beginBlock('Setting up FlexRAN service')
-      vduHelper.configureSystemInfo('FlexRAN Controller', 'This is the FlexRAN Controller of the SimulaMet FlexRAN VNF!')
-      vduHelper.createFileFromString('/lib/systemd/system/flexran.service', """\
-[Unit]
-Description=FlexRAN Controller
-After=ssh.target
-
-[Service]
-ExecStart=/bin/sh -c 'exec /usr/bin/env FLEXRAN_RTC_HOME=/home/nornetpp/src/{gitDirectory}/flexran FLEXRAN_RTC_EXEC=/home/nornetpp/src/{gitDirectory}/flexran/build ./build/rt_controller -c log_config/basic_log >>/var/log/flexran.log 2>&1'
-KillMode=process
-Restart=on-failure
-RestartPreventExitStatus=255
-WorkingDirectory=/home/nornetpp/src/{gitDirectory}/flexran
-
-[Install]
-WantedBy=multi-user.target
-""".format(gitDirectory = gitDirectory))
-
-      vduHelper.createFileFromString('/home/nornetpp/log',
-"""\
-#!/bin/sh
-tail -f /var/log/flexran.log
-""", True)
-
-      vduHelper.createFileFromString('/home/nornetpp/restart',
-"""\
-#!/bin/sh
-DIRECTORY=`dirname $0`
-sudo service flexran restart && $DIRECTORY/log
-""", True)
-      vduHelper.runInShell('sudo chown nornetpp:nornetpp /home/nornetpp/log /home/nornetpp/restart')
-      vduHelper.endBlock()
-
-      # ====== Set up sysstat service =======================================
-      vduHelper.installSysStat()
-
-      # ====== Clean up =====================================================
-      vduHelper.cleanUp()
-
-      message = vduHelper.endBlock()
-      function_set( { 'outout': message } )
-      set_flag('flexrancharm.configured-flexran')
-   except:
-      message = vduHelper.endBlockInException()
-      function_fail(message)
-   finally:
-      clear_flag('actions.configure-flexran')
-
-
-# ###### restart-flexran function ###############################################
-@when('actions.restart-flexran')
-@when('flexrancharm.configured-flexran')
-def restart_flexran():
-   vduHelper.beginBlock('restart_flexran')
-   try:
-
-      vduHelper.runInShell('sudo service flexran restart')
-
-      message = vduHelper.endBlock()
-      function_set( { 'outout': message } )
-   except:
-      message = vduHelper.endBlockInException()
-      function_fail(message)
-   finally:
-      clear_flag('actions.restart-flexran')
-
-
-
-# ###########################################################################
-# #### P4-OvS Charm functions                                           ####
+# #### P4-OvS Charm functions                                            ####
 # ###########################################################################
 
 # ###### Installation #######################################################
@@ -241,7 +66,7 @@ def install_p4ovscharm_proxy_charm():
    vduHelper.setStatus('install_p4ovscharm_proxy_charm: SSH proxy charm is READY')
 
 
-# ###### prepare-p4ovs-build function #####################################
+# ###### prepare-p4ovs-build function #######################################
 @when('actions.prepare-p4ovs-build')
 @when('p4ovscharm.installed')
 @when_not('p4ovscharm.prepared-p4ovs-build')
@@ -249,10 +74,7 @@ def prepare_p4ovs_build():
    vduHelper.beginBlock('prepare_p4ovs_build')
    try:
 
-      # ====== Get P4-OvS parameters ===========================================
-      # For a documentation of the installation procedure, see:
-      # https://gitlab.eurecom.fr/mosaic5g/mosaic5g/-/wikis/tutorials/slicing
-
+      # ====== Get P4-OvS parameters ========================================
       gitRepository = function_get('p4ovs-git-repository')
       gitCommit     = function_get('p4ovs-git-commit')
       gitDirectory  = 'P4-OvS'
@@ -260,7 +82,7 @@ def prepare_p4ovs_build():
       # ====== Prepare system ===============================================
       vduHelper.beginBlock('Preparing system')
       vduHelper.testNetworking()
-      vduHelper.waitForPackageUpdatesToComplete()
+      # vduHelper.waitForPackageUpdatesToComplete()
       vduHelper.aptInstallPackages([
          'automake',
          'bison',
@@ -368,20 +190,17 @@ sudo make install""")
       clear_flag('actions.configure-pi')
 
 
-# ###### configure-p4ovs function #########################################
+# ###### configure-p4ovs function ###########################################
 @when('actions.configure-p4ovs')
 @when('p4ovscharm.configured-pi')
 def configure_p4ovs():
    vduHelper.beginBlock('configure_p4ovs')
    try:
 
-      # ====== Get P4-OvS parameters =======================================
-      # For a documentation of the installation procedure, see:
-      # https://gitlab.eurecom.fr/mosaic5g/mosaic5g/-/wikis/tutorials/slicing
-
+      # ====== Get P4-OvS parameters ========================================
       gitDirectory = 'P4-OvS'
 
-      # ====== Build P4-OvS ================================================
+      # ====== Build P4-OvS =================================================
       vduHelper.beginBlock('Building P4-OvS itself')
       vduHelper.executeFromString("""\
 export MAKEFLAGS="-j`nproc`" && \\
@@ -391,13 +210,13 @@ cd /home/nornetpp/src/{gitDirectory} && \\
 make""".format(gitDirectory = gitDirectory))
       vduHelper.endBlock()
 
-      # ====== Configure P4-OvS ================================================
+      # ====== Configure P4-OvS =============================================
       vduHelper.beginBlock('Configuring P4-OvS')
       vduHelper.executeFromString("""\
 cd /home/nornetpp/src/{gitDirectory}""".format(gitDirectory = gitDirectory))
       vduHelper.endBlock()
 
-      # ====== Set up P4-OvS service ===========================================
+      # ====== Set up P4-OvS service ========================================
       vduHelper.beginBlock('Setting up P4-OvS service')
       vduHelper.configureSystemInfo('P4-OvS', 'This is the SimulaMet P4-OvS VNF!')
       #vduHelper.createFileFromString('/lib/systemd/system/p4ovs.service', """\
@@ -447,7 +266,7 @@ sudo service p4ovs restart && $DIRECTORY/log
       clear_flag('actions.configure-p4ovs')
 
 
-# ###### restart-p4ovs function ###############################################
+# ###### restart-p4ovs function #############################################
 @when('actions.restart-p4ovs')
 @when('p4ovscharm.configured-p4ovs')
 def restart_p4ovs():
